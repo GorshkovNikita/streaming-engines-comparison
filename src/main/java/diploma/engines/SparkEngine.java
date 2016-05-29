a104d06f58a8ae0bdd6fdb84d34b5ba08fba65c2
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-05-29 20:18:06 +0300

    Changed approach of Kafka config

--- src/main/java/diploma/engines/SparkEngine.java
+++ src/main/java/diploma/engines/SparkEngine.java
@@ -2,12 +2,15 @@ package diploma.engines;
 
 import diploma.processors.Processor;
 import diploma.spark.CustomReceiver;
+import kafka.serializer.StringDecoder;
 import org.apache.spark.*;
+import org.apache.spark.network.protocol.Encoders;
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import scala.Tuple2;
+import scala.collection.DefaultMap;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
@@ -15,7 +18,9 @@ import twitter4j.TwitterObjectFactory;
 import javax.swing.event.InternalFrameEvent;
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Никита on 06.04.2016.
@@ -39,11 +44,24 @@ public class SparkEngine extends AbstractEngine implements Serializable {
 //                    "~/diploma/streaming-comparison-engines/target/streaming-engines-comparison-1.0-jar-with-dependencies.jar"
 //            });
 
-        Map<String, Integer> topics = new HashMap<>();
-        topics.put("my-replicated-topic", 1);
         JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(1));
-        JavaPairReceiverInputDStream<String, String> messages =
-                KafkaUtils.createStream(ssc, "192.168.1.21:2181", "tweets-consumer", topics, StorageLevel.MEMORY_ONLY());
+
+        // TODO: сделать так, чтобы Spark читал сообщения с начала (свойство kafka consumer auto.offset.reset smallest)
+//        Map<String, Integer> topics = new HashMap<>();
+//        topics.put("my-replicated-topic", 1);
+//        JavaPairReceiverInputDStream<String, String> messages =
+//                KafkaUtils.createStream(ssc, "192.168.1.21:2181", "tweets-consumer", topics, StorageLevel.MEMORY_ONLY());
+
+        Set<String> topics = new HashSet<>();
+        topics.add("my-replicated-topic");
+
+        Map<String, String> kafkaParams = new HashMap<>();
+        kafkaParams.put("auto.offset.reset", "smallest");
+        kafkaParams.put("zookeeper.connect", "192.168.1.21:2181");
+
+        JavaPairInputDStream<String, String> messages =
+                KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class,
+                        kafkaParams, topics);
 
         JavaDStream<Status> statuses = messages.map((status) -> {
             try {

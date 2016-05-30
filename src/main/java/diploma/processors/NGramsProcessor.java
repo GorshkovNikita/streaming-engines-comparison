a2e597753e459a8021a580f2172103fffef9111f
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-05-31 02:05:12 +0300

    Changed parameter type in NGramsProcessor

--- src/main/java/diploma/processors/NGramsProcessor.java
+++ src/main/java/diploma/processors/NGramsProcessor.java
@@ -10,12 +10,12 @@ import java.util.List;
 /**
  * Created by Никита on 30.05.2016.
  */
-public class NGramsProcessor implements Processor<List<String>, Status>, Serializable {
+public class NGramsProcessor implements Processor<List<String>, String>, Serializable {
     @Override
-    public List<String> process(Status status) {
+    public List<String> process(String statusText) {
         List<String> ngrams = new ArrayList<>();
         for (int i = 1; i <= 4; i++)
-            ngrams.addAll(NGrams.ngrams(i, status.getText()));
+            ngrams.addAll(NGrams.ngrams(i, statusText));
         return ngrams;
     }
 }

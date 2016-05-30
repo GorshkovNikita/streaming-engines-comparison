6e18faccdcd6506e4442a0c1b0464ec00e20fb1a
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-05-30 14:02:24 +0300

    PrinterStringProcessor is now Serializable

--- src/main/java/diploma/processors/PrinterStringProcessor.java
+++ src/main/java/diploma/processors/PrinterStringProcessor.java
@@ -1,9 +1,11 @@
 package diploma.processors;
 
+import java.io.Serializable;
+
 /**
  * Created by Никита on 30.05.2016.
  */
-public class PrinterStringProcessor implements Processor<Void, String> {
+public class PrinterStringProcessor implements Processor<Void, String>, Serializable {
     @Override
     public Void process(String string) {
         System.out.println(string);

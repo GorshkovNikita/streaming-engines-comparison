8a44605a5dc22f6d050e346c432bd092508e81e0
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-05-31 13:50:52 +0300

    Added time count to NGramDetectionBolt

--- src/main/java/diploma/storm/NGramDetectionBolt.java
+++ src/main/java/diploma/storm/NGramDetectionBolt.java
@@ -20,9 +20,12 @@ public class NGramDetectionBolt extends AbstractBasicBolt {
 
     @Override
     public void execute(Tuple input, BasicOutputCollector collector) {
+        long start = System.nanoTime();
         List<String> ngrams = (List<String>) processor.process(input.getValueByField("statusText"));
         for (String ngram : ngrams)
             collector.emit(new ArrayList<Object>() {{ add(ngram); }});
+        long elapsedTime = System.nanoTime() - start;
+        System.out.println("Поиск ngram занял " + elapsedTime / 1000000 + "ms");
     }
 
     @Override

10f2a8ad31fa5fd473aca018a5842be927cb16f1
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-06-16 02:51:50 +0300

    Added Serializable to StatusFilterProcessor

--- src/main/java/diploma/processors/StatusFilterProcessor.java
+++ src/main/java/diploma/processors/StatusFilterProcessor.java
@@ -4,12 +4,12 @@ import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 
 /**
  * Created by Никита on 16.06.2016.
  */
-public class StatusFilterProcessor implements Processor<Status, String> {
+public class StatusFilterProcessor implements Processor<Status, String>, Serializable {
     @Override
     public Status process(String statusJson) {
         try {
